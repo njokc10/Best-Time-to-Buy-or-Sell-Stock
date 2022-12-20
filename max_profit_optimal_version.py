845d0c9fd7a6f5af05778ac42b7f1a5a5ff0f8dc
Anej Mužič <anej.muzic@gmail.com>
2022-12-20 19:21:45 +0100

    debugging

--- max_profit_optimal_version.py
+++ max_profit_optimal_version.py
@@ -1,6 +1,6 @@
 def maxProfit(cene, k):
     '''Vrne maksimalen dobicek, ki ga lahko ustvarimo
-        z kupovanjem in pordajanjem dolocene delnice
+        z nakopom in prodajo dolocene delnice
         z najvec k transakcijami.'''
 
     st_dni = len(cene)
@@ -9,24 +9,19 @@ def maxProfit(cene, k):
         return 0
     
     # k je dovolj velik, da pokrije vse moznosti
-    # if k > st_dni / 2 -> casovna zahtevnost je O(n)
     if k >= st_dni / 2:
         return sum(i-j for i, j in zip(cene[1:], cene[:-1]) if i-j > 0)
     
-    
-    # casovna zahtevnost je O(k * n-1)
     globalni_dobicek = [0]*st_dni
     for j in range(k):
-
         lokalni_dobicek = 0
         for i in range(1,st_dni):
-            # Kupimo delnico na dan i-1, prodamo delnico na dan i
+            # kupimo delnico na dan i-1, prodamo delnico na dan i
             profit = cene[i] - cene[i-1]
-            # globalni_dobicek[i] -> primerjam z profitom na i-ti dan z j-1 transakcijami
-            # lokalni_dobicek+profit -> delnico prodamo na i-ti dan 
+            # primerjamo lokalni dobicek do dneva i-1 + profit z globalnim dobickom
+            # na dan i z j-1 transakcijami.
             lokalni_dobicek = max(lokalni_dobicek+profit, globalni_dobicek[i])
-            # globalni_dobicek[i-1] -> profit z j transakcijami na i-1 dan
-            # lokalni_dobicek -> profit na i-ti dan z j transakcijami
+            # primerjamo globalni dobicek na dan i-1 z globalnim dobickom na dan i z j transakcijami
             globalni_dobicek[i] = max(globalni_dobicek[i-1], lokalni_dobicek)
 
     return globalni_dobicek[-1]
